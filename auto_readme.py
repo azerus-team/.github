cb2ccf3938b6a13951e28123dc3a454ffef3b559
Beksultan <96925396+Vazno@users.noreply.github.com>
2022-10-09 15:30:25 +0600

    Fixed bug, if you add more then 2 maps at the same time, it won't break

--- auto_readme.py
+++ auto_readme.py
@@ -3,12 +3,9 @@ import os
 import re
 from urllib.request import Request, urlopen
 
-
 README_PATH = os.path.join("profile", "README.md")
 with open(README_PATH, "r") as f:
 	README = f.read()
-
-
 
 # Storing json from API
 req = Request('https://azerusteam.com/api/maps', headers={'User-Agent': 'Mozilla/5.0'})
@@ -19,18 +16,21 @@ j = json.load(webpage)
 if len(j["maps"]) == len(re.findall('class=\"map\"', README)):
 	print("Up to date.")
 else:
-	a = "https://azerusteam.com/attachments/"
-
 	AUTOMATE = r'''<p class="automate" align="center">'''
-	LAST_MAP = j["maps"][0]
-	TITLE = LAST_MAP["title"]
-	mapUuid = LAST_MAP["mapUuid"]
-	versionUuid = LAST_MAP["versionUuid"]
-	MAP_URL = "https://azerusteam.com/map/" + LAST_MAP["vanity_url"]
+	map = r'''<a class="map" href=".*"><img alt=".*" width="256px" src="https://azerusteam.com/attachments/.*/.*/logo-web.png" /></a>\n'''
+	FIXED_README = README
+	FIXED_README = re.sub(map, "", FIXED_README) # Deleting previous maps from readme
+	
+	# Adding all maps to readme
+	for i in range(len(j["maps"])-1, -1, -1):
+		LAST_MAP = j["maps"][i]
+		TITLE = LAST_MAP["title"]
+		mapUuid = LAST_MAP["mapUuid"]
+		versionUuid = LAST_MAP["versionUuid"]
+		MAP_URL = "https://azerusteam.com/map/" + LAST_MAP["vanity_url"]
 
-	new_line = AUTOMATE + f'''\n<a class="map" href="{MAP_URL}"><img alt="{TITLE}" width="256px" src="https://azerusteam.com/attachments/{mapUuid}/{versionUuid}/logo-web.png" /></a>'''
-
+		new_line = AUTOMATE + f'''\n<a class="map" href="{MAP_URL}"><img alt="{TITLE}" width="256px" src="https://azerusteam.com/attachments/{mapUuid}/{versionUuid}/logo-web.png" /></a>'''
+		FIXED_README = re.sub(AUTOMATE, new_line, FIXED_README)
 
 	with open(README_PATH, "r+") as f:
-		replaced = re.sub(AUTOMATE, new_line, README)
-		f.write(replaced)
+		f.write(FIXED_README)
