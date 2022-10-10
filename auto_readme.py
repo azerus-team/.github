681eea00ae68e5598ebfb9c398e37da153bf7c35
Beksultan <96925396+Vazno@users.noreply.github.com>
2022-10-10 11:54:25 +0600

    Refactored code, added variables to control image sizes, urls

--- auto_readme.py
+++ auto_readme.py
@@ -3,34 +3,61 @@ import os
 import re
 from urllib.request import Request, urlopen
 
+IMAGE_WIDTH = 256
+SITE_URL = "https://azerusteam.com"
+DISCORD_URL = "https://azerus.team/discord"
+YOUTUBE_URL = "https://azerus.team/youtube"
+####################################################################
+
+AUTOMATE = r'''<p class="automate" align="center">'''
+
+map_hyperlink = rf'''<a class="map" href=".*"><img alt=".*" width=".*px" src="{SITE_URL}/attachments/.*/.*/logo-web.png" /></a>\n'''
+site_hyperlink = r'''<a class="site" href=".*" >'''
+discord_hyperlink = r'<a class="discord" href=".*" >'
+youtube_hyperlink = r'<a class="youtube" href=".*" >'
+
+
 README_PATH = os.path.join("profile", "README.md")
-with open(README_PATH, "r") as f:
-	README = f.read()
 
-# Storing json from API
-req = Request('https://azerusteam.com/api/maps', headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req)
-j = json.load(webpage)
+UPDATING_MESSAGE = "[Updating README.md]"
+SUCCESS_MESSAGE = "[README.md has been successfully updated by API]"
 
+def get_json() -> json:
+	req = Request(f'{SITE_URL}/api/maps', headers={'User-Agent': 'Mozilla/5.0'})
+	return json.load(urlopen(req))
 
-if len(j["maps"]) == len(re.findall('class=\"map\"', README)):
-	print("Up to date.")
-else:
-	AUTOMATE = r'''<p class="automate" align="center">'''
-	hyperlink = r'''<a class="map" href=".*"><img alt=".*" width="256px" src="https://azerusteam.com/attachments/.*/.*/logo-web.png" /></a>\n'''
-	FIXED_README = README
-	FIXED_README = re.sub(hyperlink, "", FIXED_README) # Deleting previous maps from readme
+def main():
+	print(UPDATING_MESSAGE)
+	with open(README_PATH, "r") as f:
+		README = f.read()
+
+	# Storing json from API
+	j = get_json()
+
+	# Deleting previous map hyperlinks from readme
+	README = re.sub(map_hyperlink, "", README) 
 	
+	# Replacing URLs to current URLs
+	README = re.sub(site_hyperlink, f'<a class="site" href="{SITE_URL}" >', README)
+	README = re.sub(discord_hyperlink, f'<a class="discord" href="{DISCORD_URL}" >', README)
+	README = re.sub(youtube_hyperlink, f'<a class="youtube" href="{YOUTUBE_URL}" >', README)
+
+
 	# Adding all maps to readme
 	for i in range(len(j["maps"])-1, -1, -1):
 		LAST_MAP = j["maps"][i]
 		TITLE = LAST_MAP["title"]
 		mapUuid = LAST_MAP["mapUuid"]
 		versionUuid = LAST_MAP["versionUuid"]
-		MAP_URL = "https://azerusteam.com/map/" + LAST_MAP["vanity_url"]
-
-		new_line = AUTOMATE + f'''\n<a class="map" href="{MAP_URL}"><img alt="{TITLE}" width="256px" src="https://azerusteam.com/attachments/{mapUuid}/{versionUuid}/logo-web.png" /></a>'''
-		FIXED_README = re.sub(AUTOMATE, new_line, FIXED_README)
+		MAP_URL = f"{SITE_URL}/map/{LAST_MAP['vanity_url']}"
+		new_hyperlink = f'''\n<a class="map" href="{MAP_URL}"><img alt="{TITLE}" width="{IMAGE_WIDTH}px" src="{SITE_URL}/attachments/{mapUuid}/{versionUuid}/logo-web.png" /></a>'''
+		new_line = AUTOMATE + new_hyperlink
+		
+		README = re.sub(AUTOMATE, new_line, README)
 
 	with open(README_PATH, "r+") as f:
-		f.write(FIXED_README)
+		f.write(README)
+		print(SUCCESS_MESSAGE)
+
+if __name__ == "__main__":
+	main()
--- profile/README.md
+++ profile/README.md
@@ -1,5 +1,5 @@
 <p align="center">
-  <a href="https://azerusteam.com/"><img src="https://azerusteam.com/images/logo.png">
+  <a class="site" href="https://azerusteam.com" > <img src="https://azerusteam.com/images/logo.png">
 </p>
 
 <hr></hr>
@@ -30,12 +30,12 @@ Our maps are officially distributed through Realms for:
 
 <h6>Here you can find our works and download some of them or join us!</h6>
 <hr></hr>
-<a href="https://azerusteam.com/">
+<a class="site" href="https://azerusteam.com" >
   <img align="left" alt="Site" width="20px" src="https://yt3.ggpht.com/ytc/AMLnZu_nPZjZm45DccQf0d2lqaZCnDI4kgueZkV7VSjO=s88-c-k-c0x00ffffff-no-rj" />
 </a>
-<a href="https://discord.gg/Wp8rnkQ">
+<a class="discord" href="https://azerus.team/discord" >
   <img align="left" alt="Discord" width="20px" src="https://www.svgrepo.com/show/353655/discord-icon.svg" />
 </a>
-<a href="https://www.youtube.com/channel/UC3w8DyozZCBv71WAXfDJhfQ">
+<a class="youtube" href="https://azerus.team/youtube" >
   <img align="left" alt="Youtube" width="30px" src="https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg" />
 </a>
